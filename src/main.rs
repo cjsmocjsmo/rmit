2cadba1769f4754a033c37930c3b114e7e909ed9
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-28 21:54:05 -0800

    Refactor file removal logic and remove empty directories

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-// use std::fs;
+use std::path::Path;
 use walkdir::WalkDir;
+use std::fs;
 
 fn main() {
     // let img_path = "/media/pipi/0123-4567/Images".to_string();
@@ -8,8 +9,12 @@ fn main() {
     let apath = "/media/pi/USBMOVIES/Master_HPics/".to_string();
     let mut count = 0;
     let mut removed = 0;
+    let rm_vec = [
+        "gz", "zip", "ZIP", "txt", "ps1", "htm", "gif", "jar", "dat", "jpgblk", "key",
+        "tif", "MPG", "AVI", "DS_Store", "bz2", "3gp", "avi", "mp4", "pdf", "mp3", "nfo", "m3u",
+    ];
 
-    for e in WalkDir::new(apath)
+    for e in WalkDir::new(apath.clone())
         .follow_links(true)
         .into_iter()
         .filter_map(|e| e.ok())
@@ -17,14 +22,43 @@ fn main() {
         if e.metadata().unwrap().is_file() {
             count += 1;
             let fname = e.path().to_string_lossy().to_string();
-            if fname.contains("Copy_") {
-                //remove fname
+            let ext_split = &fname.split(".").collect::<Vec<&str>>();
+            let ext = ext_split.last().unwrap();
+            if rm_vec.contains(&ext) {
+                println!("Removed: {}", &fname);
+                std::fs::remove_file(fname.clone()).unwrap();
                 removed += 1;
-                let _rmove = std::fs::remove_file(fname.clone()).unwrap();
+            }
+
+
+            
+        }
+    }
+    remove_empty_dirs(Path::new(&apath.clone())).unwrap();
+    println!("Start count: {}\nFiles removed: {}", count, removed.clone());
+}
+
+fn remove_empty_dirs(root_dir: &Path) -> std::io::Result<()>{
+    for entry in root_dir.read_dir()? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            if path.read_dir()?.next().is_none() {
+                fs::remove_dir_all(path)?;
             }
         }
     }
-    println!("Start count: {}\nFiles removed: {}", count, removed.clone());
+    // for entry in root_dir.walk() {
+    //     match entry {
+    //         Ok(dir) => {
+    //             if dir.is_dir() && dir.read_dir()?.next().is_none() {
+    //                 fs::remove_dir_all(dir)?;
+    //             }
+    //         },
+    //         Err(error) => return Err(error),
+    //     }
+    // }
+    Ok(())
 }
 
 // pub fn rm_unwanted_files(apath: String) -> i32 {
